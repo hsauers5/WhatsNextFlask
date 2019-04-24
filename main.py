d52b9565bc8074693ff3c826dce0b6f8e6d7fc1f
hsauers5 <hsauers5@gmail.com>
2019-04-23 20:45:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 # just serve all the static files under root
 app = Flask(__name__, static_folder='.', static_url_path='', template_folder='')
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-# hold user's data
-category = ""
-price = ""
-distance = ""
-location = ""
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,26 +12,26 @@ def home():
 
 @app.route('/categories', methods=['GET'])
 def categories():
-  location = request.args['location']
+  session['location'] = request.args['location'] 
   return render_template('/categories.html')
 
 
 @app.route('/prices', methods=['GET'])
 def prices():
-  category = request.args['category']
+  session['category'] = request.args['category']
   return render_template('/prices.html')
 
 
 @app.route('/radius', methods=['GET'])
 def radius():
-  price = request.args['price']
+  session['price'] = request.args['price']
   return render_template('distance.html')
 
 
 @app.route('/restaurants', methods=['GET'])
 def restaurants():
-  distance = request.args['distance']
-  return get_restaurants(location=location, category=category, radius=distance, price=price)
+  session['distance'] = request.args['distance']
+  return get_restaurants(location=session['location'], category=session['category'], radius=session['distance'], price=session['price'])
 
 
 # generate restaurants list from api
@@ -43,9 +40,9 @@ def get_restaurants(location="32816", category="asian", radius="5", price="2"):
   # build html
   return  """
           <html><body><h1>RESTAURANTS</h1></body></html>
-          """ + str({category, price, distance, radius})
+          """ + str({category, price, location, radius})
   
   
 # start listening
 if __name__ == "__main__":
-    app.run(debug=True, port='8080', host='0.0.0.0')
+  app.run(debug=True, port='8080', host='0.0.0.0')
